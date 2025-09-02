ff5b4d10935b7fd28897ea55d56778ca0ec5a890
adhyajain-maker <adhya_jain@s2024.ssts.edu.sg>
2025-09-02 20:53:16 +0800

    initial project

---
+++ Graded/Graded/GradedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradedApp.swift
+//  Graded
+//
+//  Created by Adhya  on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GradedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Graded/GradedTests/GradedTests.swift
@@ -0,0 +1,17 @@
+//
+//  GradedTests.swift
+//  GradedTests
+//
+//  Created by Adhya  on 2/9/25.
+//
+
+import Testing
+@testable import Graded
+
+struct GradedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
